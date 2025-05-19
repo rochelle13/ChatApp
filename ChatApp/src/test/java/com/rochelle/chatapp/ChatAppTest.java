eed192f568a164a330ec7e8d8050416cbff57a14
ST10290935 <128598477+ST10290935@users.noreply.github.com>
2025-05-19 19:02:45 +0200

    Update ChatAppTest.java

--- ChatApp/src/test/java/com/rochelle/chatapp/ChatAppTest.java
+++ ChatApp/src/test/java/com/rochelle/chatapp/ChatAppTest.java
@@ -1,15 +1,10 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/UnitTests/JUnit5TestClass.java to edit this template
- */
 package com.rochelle.chatapp;
 
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 /**
- *
- * @author 27790
+ * Unit tests for the Login class.
  */
 public class ChatAppTest {
 
@@ -22,7 +17,7 @@ public class ChatAppTest {
     @Test
     public void testInvalidUsernameSpecialChar() {
         Login login = new Login("username", "Password1@");
-        assertFalse(login.checkUsername(), "Username is invalid because it does not contains '$'");
+        assertFalse(login.checkUsername(), "Username is invalid because it does not contain '$'");
     }
 
     @Test
@@ -30,55 +25,54 @@ public class ChatAppTest {
         Login login = new Login("username$12345", "Password1@");
         assertFalse(login.checkUsername(), "Username is invalid because it is too long");
     }
-    
+
     @Test
-    public void testInvalidPasswordDigit(){
-         Login login = new Login("user$name", "Password@");
+    public void testInvalidPasswordDigit() {
+        Login login = new Login("user$name", "Password@");
         assertFalse(login.checkPassword(), "Password is invalid because it is missing a digit");
     }
 
     @Test
-    public void testInvalidPasswordLength(){
-         Login login = new Login("user$name", "Pass@1");
+    public void testInvalidPasswordLength() {
+        Login login = new Login("user$name", "Pass@1");
         assertFalse(login.checkPassword(), "Password is invalid because it is too short");
     }
-    
-    @Test
-    public void testInvalidPasswordUpperCase(){
-         Login login = new Login("user$name", "password@1");
-        assertFalse(login.checkPassword(), "Password is invalid because it is missing an uppercase letter");
 
-    }
-    
     @Test
-    public void testLoginUserSuccess(){
-        //test to check if login credentials are valid
+    public void testInvalidPasswordUpperCase() {
+        Login login = new Login("user$name", "password@1");
+        assertFalse(login.checkPassword(), "Password is invalid because it is missing an uppercase letter");
+    }
+
+    @Test
+    public void testLoginUserSuccess() {
         String username = "user$name";
         String password = "Password1@";
         Login login = new Login(username, password);
         boolean loginStatus = login.loginUser(username, password);
         assertTrue(loginStatus, "Login is successful because credentials are valid");
     }
-    
+
     @Test
-    public void testLoginUserFailure(){
+    public void testLoginUserFailure() {
         String username = "user$name";
         String password = "Password1@";
         Login login = new Login(username, password);
-        boolean loginStatus = login.loginUser("wrong$name","wrongPassword");
+        boolean loginStatus = login.loginUser("wrong$name", "wrongPassword");
         assertFalse(loginStatus, "Login is unsuccessful because credentials are invalid");
-    }   
+    }
 
     @Test 
-    public void testReturnLoginSuccess(){
+    public void testReturnLoginSuccess() {
         Login login = new Login("user$name", "Password1@");
         String message = login.returnLogin(true);
         assertEquals("Welcome user", message, "Return Login should welcome the user");
     }
-    
-     @Test 
-    public void testReturnLoginFailure(){
-        
-    }
 
+    @Test 
+    public void testReturnLoginFailure() {
+        Login login = new Login("user$name", "Password1@");
+        String message = login.returnLogin(false);
+        assertEquals("Login failed", message, "Return Login should indicate failure");
+    }
 }
